2150979214aaa911ae3defd7f8cd3660c8bb4f3e
Suyash jhawer <suyashjhawer@gmail.com>
2021-05-10 21:02:11 +0530

    More Feat

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -12,7 +12,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degress out."+" It feels like "+body.current.weather_descriptions.feelslike+". Humidity is "+body.current.weather_descriptions.humidity+".")
+            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degress out."+" It feels like "+body.current.feelslike+". Humidity is "+body.current.humidity+".")
         }
     })
 }
